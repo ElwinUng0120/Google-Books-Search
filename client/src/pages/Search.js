67764ac2f13366ce0f019efb94adbaceca00d91d
Elwin Ung <elwin.ung@gmail.com>
2020-11-21 19:39:04 -0500

    Checking api call

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -18,8 +18,7 @@ function Search(){
 
     async function getAPIKEY(){
         const response = await API.getAPIKEY();
-        console.log(response);
-        setAPI_KEY(response);
+        setAPI_KEY(response.data);
     }
 
     function handleInputChange(event){
